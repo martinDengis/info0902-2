46df27e1bd7f93c538484f36573c322722a53614
Judfraises2001 <74183082+Judfraises2001@users.noreply.github.com>
2023-04-24 15:02:58 +0200

    Update PointDctBST2d.c
    PointDctBST2d.c

--- src/PointDctBST2d.c
+++ src/PointDctBST2d.c
@@ -8,3 +8,42 @@
 #include "BST2d.h"
 
 // A compléter
+struct PointDct_t{
+    BST2d *bst2d;
+};
+
+int cmpPoint(void *p1, void *p2);
+
+PointDct *pdctCreate(List *lpoint, List lvalues){
+    PointDct *pdctbst2d = malloc(sizeof(PointDct));
+    if(!pdctbst2d){
+        printf("pdctCreate: allocation error");
+        return NULL;
+    }
+
+    BST2d *bst2dtoDct = bst2dNew(cmpPoint);
+    for(LNode *p = lpoints->head, *v = lvalues->head; p != NULL, v != NULL; p = p->next, v = v->next){
+        bst2dInsert(bst2dtoDct, p->value, v->value);
+    }
+
+    pdctbst2d->bst2d = bst2dtoDct;
+    return pdctbst2d;
+}
+
+
+void pdctFree(PointDct *pd){
+    bst2dFree(pd->bst2d);
+    free(pd);
+}
+
+size_t pdctSize(PointDct *pd){
+    return bst2dSize(pd->bst2d);
+}
+
+void *pdctExactSearch(PointDct *pd, Point *p){
+    bst2dSearch(pd->bst2d, p);
+}
+
+List *pdctBallSearch(PointDct *pd, Point *p, double r){
+    return bst2dBallSearch(pd->bst2d, p, r);
+}
